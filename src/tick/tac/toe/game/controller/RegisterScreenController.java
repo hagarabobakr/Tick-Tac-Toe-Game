e3640736a7d32b5bc3879f3e7a21726aca9453cc
sammar <sammaralaa95@gmail.com>
2024-07-26 15:42:27 +0300

    first modification on regstirController

--- src/tick/tac/toe/game/controller/RegisterScreenController.java
+++ src/tick/tac/toe/game/controller/RegisterScreenController.java
@@ -34,24 +34,8 @@ import javafx.stage.Stage;
  */
 public class RegisterScreenController implements Initializable {
 
-    @FXML
-    private AnchorPane Anchorpn;
-    @FXML
-    private VBox vbox;
-    @FXML
-    private GridPane gridpn;
-    @FXML
-    private Label namelabel;
-    @FXML
-    private Label passwordlabel;
-    @FXML
-    private Label confirmlabel;
-    @FXML
-    private ImageView nameimg;
-    @FXML
-    private ImageView passwordimg;
-    @FXML
-    private ImageView confirmimg;
+    
+   
     @FXML
     private TextField namefield;
     @FXML
@@ -68,59 +52,26 @@ public class RegisterScreenController implements Initializable {
     private String password = null;
     private String confirmedPassword = null;
     
-    private void SendRegisterRequest(){
-        register.setOnAction(event -> {
-                if(namefield.getText() != null){
-                    if(validation(namefield.getText())){
-                        username = namefield.getText();
-                    }
-                    else{
-                        showAlert(AlertType.ERROR, "invalid data", "username must contain both letters and numbers.");
-                        return;
-                    }
-                }
-                else{
-                    showAlert(AlertType.ERROR, "uncomplete data", "Please fill namefield");
-                    return;
-                }
-                if(passwordfield.getText() != null){
-                    if(validation(passwordfield.getText())){
-                        password = passwordfield.getText();
-                    }
-                    else{
-                        showAlert(AlertType.ERROR, "invalid data", "password must contain both letters and numbers.");
-                        return;
-                    }
-                }
-                else{
-                    showAlert(AlertType.ERROR, "uncomplete data", "Please fill passwordfield");
-                    return;
-                }
-                if(confirmfield.getText() != null){
-                     confirmedPassword = confirmfield.getText();
-                }
-                else{
-                    showAlert(AlertType.ERROR, "uncomplete data", "Please fill confirm passwordfield");
-                    return;
-                }
-                if(password.equals(confirmedPassword) == true){
-                   //send register request to server 
-                }
-                else{
-                    showAlert(AlertType.ERROR, "wrong data", "be sure that confirmedPassword is the same of password");
-                    return;
-                }
-        });
-         
-    }
-    private boolean validation(String name) {
-        // Check if the name contains both letters and numbers
-        boolean hasLetter = name.matches(".*[a-zA-Z]+.*");
-        boolean hasNumber = name.matches(".*[0-9]+.*");
-        return hasLetter && hasNumber;
-    }
-    
+   
     @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        username=namefield.getText();
+        password = passwordfield.getText();
+        confirmedPassword = confirmfield.getText();
+        if(!(username.equals("") && password.equals("") && confirmedPassword.equals(""))){
+            if(password.equals(confirmedPassword)){
+                
+            }else{
+                //unmatched passwords
+            }
+        }else{
+            //empty fields
+        }
+        if (event.getSource() == register) {
+            changeScene(event, "/tick/tac/toe/game/view/OnlinePlayersListScreen.fxml");
+        }
+    }
+   
     private void showAlert(AlertType type, String title, String message) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
@@ -128,14 +79,14 @@ public class RegisterScreenController implements Initializable {
         alert.setContentText(message);
         alert.showAndWait();
     }
-    
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException {
-        if (event.getSource() == register) {
-            changeScene(event, "/tick/tac/toe/game/view/OnlinePlayersListScreen.fxml");
-        }
+     private boolean validation(String name) {
+        // Check if the name contains both letters and numbers
+        boolean hasLetter = name.matches(".*[a-zA-Z]+.*");
+        boolean hasNumber = name.matches(".*[0-9]+.*");
+        return hasLetter && hasNumber;
     }
+    
+    
     
     private void changeScene(ActionEvent event, String fxmlFile) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource(fxmlFile));
