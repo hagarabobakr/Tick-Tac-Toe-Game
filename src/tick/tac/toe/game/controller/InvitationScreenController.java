a5b7eb455fa95e8e8b8425c5f6ede2ff5b65e57c
sammar <sammaralaa95@gmail.com>
2024-07-30 19:46:53 +0300

    complete invitation and make watingForOthersControler

--- src/tick/tac/toe/game/controller/InvitationScreenController.java
+++ src/tick/tac/toe/game/controller/InvitationScreenController.java
@@ -10,41 +10,62 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import tick.tac.toe.game.network.Client;
 import tick.tac.toe.game.network.ResponseListener;
+import tick.tac.toe.game.network.requestCreator;
 
 /**
  * FXML Controller class
  *
  * @author Electronica Care
  */
-public class InvitationScreenController implements Initializable,ResponseListener {
+public class InvitationScreenController implements Initializable, ResponseListener {
 
     /**
      * Initializes the controller class.
      */
     @FXML
     private Button AcceptBtn;
-    
+
     @FXML
     private Button RefuseBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }   
+    }
+
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
         if (event.getSource() == AcceptBtn) {
-            
+            //Client.sendRequest(requestCreator.acceptInvitation());
+            changeScene(event, "/tick/tac/toe/game/view/GameBoardScreen.fxml");
+
         }
         if (event.getSource() == RefuseBtn) {
-            
+           // Client.sendRequest(requestCreator.declineInvitation(senderPlayerName));
+
         }
-        
+
     }
+
+    private void changeScene(ActionEvent event, String fxmlFile) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
     @Override
     public void onResponse(String response) {
-        
+
     }
 }
---
+++ src/tick/tac/toe/game/controller/WaitingForOthersScreenController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tick.tac.toe.game.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Electronica Care
+ */
+public class WaitingForOthersScreenController implements Initializable {
+
+    @FXML
+    private VBox Box;
+    @FXML
+    private ProgressBar proBar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
